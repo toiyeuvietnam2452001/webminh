ab04140d448ed95bb473779dbdc96cd705b2309a
toiyeuvietnam2452001 <toiyeuvietnam2452001@gmail.com>
2026-06-10 10:12:07 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'img.icons8.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
